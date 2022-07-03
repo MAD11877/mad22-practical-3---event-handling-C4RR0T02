ed950014b385327954143f9ade8197f3cbff6d2d
Ngui Jia Le Sherlena <Sherlena2002@gmail.com>
2022-07-03 18:55:27 +0800

    I am done like actually done :)))))))))))))))))))))

--- app/src/main/java/sg/edu/np/mad/madpractical/MessageGroup.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MessageGroup.java
@@ -1,8 +1,11 @@
 package sg.edu.np.mad.madpractical;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MessageGroup extends AppCompatActivity {
 
@@ -11,7 +14,32 @@ public class MessageGroup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_group);
 
+        Button grp1 = (Button)findViewById(R.id.group1);
+        Button grp2 = (Button)findViewById(R.id.group2);
 
+        grp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                // Replace the contents of the container with the new fragment
+                ft.replace(R.id.frame, new openFragment());
+                // or ft.add(R.id.your_placeholder, new FooFragment());
+                // Complete the changes added above
+                ft.commit();
+            }
+        });
+
+        grp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                // Replace the contents of the container with the new fragment
+                ft.replace(R.id.frame, new openFragment2());
+                // or ft.add(R.id.your_placeholder, new FooFragment());
+                // Complete the changes added above
+                ft.commit();
+            }
+        });
 
 
     }
